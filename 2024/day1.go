d5fa17ec996eb3e62f37e9903af43679eaad00fa
afr2903 <afr102903@gmail.com>
2024-12-21 22:57:43 -0500

    Day 1 part 2 solution

--- 2024/day1.go
+++ 2024/day1.go
@@ -50,4 +50,22 @@ func main() {
 		total_distance += abs(left[i], right[i])
 	}
 	fmt.Println("Total distance: ", total_distance)
+
+	// Second part: Find the similarity score between the two lists
+	var right_map map[int]int
+	right_map = make(map[int]int)
+	for i := 0; i < len(right); i++ {
+		if _, ok := right_map[right[i]]; ok {
+			right_map[right[i]]++
+		} else {
+			right_map[right[i]] = 1
+		}
+	}
+	similarity_score := 0
+	for i := 0; i < len(left); i++ {
+		if _, ok := right_map[left[i]]; ok {
+			similarity_score += left[i] * right_map[left[i]]
+		}
+	}
+	fmt.Println("Similarity score: ", similarity_score)
 }
