ca105b9e9f4efd210c9ca6ec6f2ee9209f027b1c
afr2903 <afr102903@gmail.com>
2024-12-27 16:09:13 -0500

    Day 3 part 2

--- 2024/day3.go
+++ 2024/day3.go
@@ -16,16 +16,23 @@ func main() {
 		return
 	}
 
-	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)`)
 	total := 0
+	enabled := true
 	scanner := bufio.NewScanner(strings.NewReader(string(file)))
 	for scanner.Scan() {
 		line := scanner.Text()
 		matches := re.FindAllStringSubmatch(line, -1)
 		for _, match := range matches {
-			num1, _ := strconv.Atoi(match[1])
-			num2, _ := strconv.Atoi(match[2])
-			total += num1 * num2
+			if match[0] == "do()" {
+				enabled = true
+			} else if match[0] == "don't()" {
+				enabled = false
+			} else if enabled {
+				num1, _ := strconv.Atoi(match[1])
+				num2, _ := strconv.Atoi(match[2])
+				total += num1 * num2
+			}
 		}
 	}
 	fmt.Println("Total: ", total)
